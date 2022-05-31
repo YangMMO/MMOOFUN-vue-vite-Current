7b35a7bb6253f8f334063c9089557df2d7bd8c2c
YangMMO <11436052@qq.com>
2022-05-31 17:58:48 +0800

    push

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/mmoofun-vite-vue',
+  // base: '/mmoofun-vite-vue',
 })
