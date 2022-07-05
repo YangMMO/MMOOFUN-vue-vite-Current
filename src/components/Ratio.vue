5663cc9c70b1e7f9532167da51a1a61ce8d0615b
YangMMO <11436052@qq.com>
2022-07-05 14:50:24 +0800

    push

--- src/components/Ratio.vue
+++ src/components/Ratio.vue
@@ -43,6 +43,18 @@
           </section>
 
         </div>
+        <div class="w-full text-center py-6">
+          <select name="" id="" class="box p8 bg-gradient-to-b from-slate-800 to-slate-800"
+            @change="clickSelect($event)">
+            <option v-for="item in floatRatio" :key="item" :selected="item.val === 100" class="text-black">
+              {{ item.text }}
+            </option>
+            <!-- <option class="text-black" value="1">0</option>
+            <option class="text-black" value="0.1">0.1</option>
+            <option class="text-black" value="0.01">0.01</option>
+            <option class="text-black" value="0.001">0.001</option> -->
+          </select>
+        </div>
         <p class="text-center mb-6 opacity-20">By MMOO.FUN</p>
       </div>
 
@@ -78,9 +90,16 @@ export default {
     // { w: 1 + 1.414, h: 1 },
     { w: 3.303, h: 1 },
 
-    {w: 0, h: 0}
+    {w: 0, h: 0},
     ]
-    let scale = commonRatio[0].w / commonRatio[0].h;
+    
+    let floatRatio = [
+      {text: '0', val: 1},
+      {text: '0.1', val: 10},
+      {text: '0.01', val: 100},
+      {text: '0.001', val: 1000},
+      {text: '0.0001', val: 10000},
+    ];
 
     return {
       datas: JSON.parse(JSON.stringify(designJson)),
@@ -91,6 +110,9 @@ export default {
       height: '',
       ratioWidth: 0,
       ratioHeight: 0,
+      currentFloat: floatRatio[2],
+      floatRatio: floatRatio,
+      currentPosition: '',
     }
   },
   methods: {
@@ -101,21 +123,30 @@ export default {
     },
     /* 计算 */
     calculate(position) {
+      this.currentPosition = position || this.currentPosition;
+      if (this.width === '' && this.height === '') {
+        return
+      }
+
       if (this.currentRatio.w !== 0) {
-        if (position === 'width') {
-          this.height = Math.round(this.width / this.currentRatio.w * this.currentRatio.h * 1000) / 1000;
+        if (this.currentPosition === 'width') {
+          this.height = Math.round(this.width / this.currentRatio.w * this.currentRatio.h * this.currentFloat.val) / this.currentFloat.val;
         } else {
-          this.width = Math.round(this.height / this.currentRatio.h * this.currentRatio.w * 1000) / 1000;
+          this.width = Math.round(this.height / this.currentRatio.h * this.currentRatio.w * this.currentFloat.val) / this.currentFloat.val;
         }
         return;
       }
 
-      if (position === 'width') {
-        this.height = Math.round(this.width / this.ratioWidth * this.ratioHeight * 100) / 100;
+      if (this.currentPosition === 'width') {
+        this.height = Math.round(this.width / this.ratioWidth * this.ratioHeight * this.currentFloat.val) / this.currentFloat.val;
       } else {
-        this.width = Math.round(this.height / this.ratioHeight * this.ratioWidth * 100) / 100;
+        this.width = Math.round(this.height / this.ratioHeight * this.ratioWidth * this.currentFloat.val) / this.currentFloat.val;
       }
     },
+    clickSelect(event) {
+      this.currentFloat = this.floatRatio.find(item => item.text === event.target.value);
+      this.calculate();
+    }
 
   },
   watch: {
